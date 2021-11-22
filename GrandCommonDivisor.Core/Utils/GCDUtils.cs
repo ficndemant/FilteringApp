b352d9f5966887510acc0174f7e2b44d2f625450
ficndemant <ficn@demant.com>
2021-11-22 18:19:39 +0100

    First version of GCD completed, probably need to handle larger numbers next

---
+++ GrandCommonDivisor.Core/Utils/GCDUtils.cs
@@ -0,0 +1,36 @@
+﻿using GrandCommonDivisor.Core.models;
+using System;
+
+namespace GrandCommonDivisor.Core
+{
+    public static class GCDUtils
+    {
+        public static int CalculateGCD(int firstN, int secondN)
+        {
+            if (secondN == 0)
+            {
+                return firstN;
+            }
+            int modulo = firstN % secondN;
+
+            return CalculateGCD(secondN, modulo);
+        }
+
+        public static NumberParseResult CheckNumber(string number)
+        {
+            int num;
+
+            if (!int.TryParse(number, out num))
+            {
+                var error = "Please try typing in again!";
+                return new NumberParseResult(error);
+            }
+            
+            return new NumberParseResult(num);
+        }
+    }
+
+
+
+    
+}
