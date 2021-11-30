83b882e8829be352774b1ea8069b8b84fca7e997
ficndemant <ficn@demant.com>
2021-11-30 19:38:03 +0100

    commented things removed, mostly

--- FilteringApp/FilteringApp.ConsoleUI/Program.cs
+++ FilteringApp/FilteringApp.ConsoleUI/Program.cs
@@ -56,7 +56,8 @@ namespace FilteringApp.ConsoleUI
 
         public static int ReadUserFilteringNumber()
         {
-            Console.WriteLine("Please provide an INT value for filtering the array.");
+            Console.WriteLine("Please provide a SINGLE DIGIT INT value for filtering the array.");
+            // ! Need to find another better way to handle more than one digit, eg. search for "12" or "123"
             var value = Console.ReadLine();
 
             return ArrayUtils.CheckFilteringValue(value);
@@ -75,51 +76,5 @@ namespace FilteringApp.ConsoleUI
         {
             Console.WriteLine($"This is the sum of array elements: {theSum}");
         }
-
-        //static void Main(string[] args)
-        //{
-        //    Console.Clear();
-        //    int[] array = new int[0];
-        //    var isArrayValid = false;
-        //    var arrayPackage = new ArrayParseResult(array);
-        //    while (isArrayValid == false)
-        //    {
-        //        Console.WriteLine("Please provide INT numbers for the array, separated by commas (for example like 1,2,3,4,...,12321,21433).");
-        //        var stringArray = Console.ReadLine().Split(',');
-        //        arrayPackage = ArrayUtils.CheckArray(stringArray);
-        //        if (arrayPackage.IsValid == false)
-        //        {
-        //            Console.WriteLine(arrayPackage.Error);
-        //        }
-        //        else
-        //        {
-        //            isArrayValid = arrayPackage.IsValid;
-        //        }
-        //    }
-        //    var intValue = 0;
-        //    var isFilteringValueValid = false;
-        //    var userValueParseResults = new UserValueParseResult(intValue);
-        //    while (isFilteringValueValid == false)
-        //    {
-        //        Console.WriteLine("Please provide an INT value for filtering the array.");
-        //        var value = Console.ReadLine();
-        //        var intValueObject = ArrayUtils.CheckFilteringValue(value);
-
-        //        if (intValueObject.IsValid == false)
-        //        {
-        //            Console.WriteLine(userValueParseResults.Error);
-        //        }
-        //        else
-        //        {
-        //            intValue = intValueObject.Value;
-        //            isFilteringValueValid = true;
-        //        }
-        //    }
-        //    var userInput = new UserInput(arrayPackage.Array, intValue);
-        //    var filteredValue = ArrayUtils.FilterOutResults(userInput);
-        //    DispplayFilteringResults(filteredValue);
-        //    DisplaySummingResults(ArrayUtils.AddArrayElements(arrayPackage.Array));
-        //}
-
     }
 }
