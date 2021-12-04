61d5746f2af884bf3c3e037f8a78cccd050bbfc7
ficndemant <ficn@demant.com>
2021-12-04 14:04:22 +0100

    throws & extensions & folders done.

--- GCD/GrandCommonDivisor.Core/Utils/GCDUtils.cs
+++ GCD/GrandCommonDivisor.Core/Utils/GCDUtils.cs
@@ -4,22 +4,6 @@ namespace GrandCommonDivisor.Core
 {
     public static class GCDUtils
     {
-        //public static int CheckInput(string number)
-        //{
-        //    int num;
-        //    if (!int.TryParse(number, out num))
-        //    {
-        //        throw new ArgumentException("Please try number again!");                   
-        //    }
-
-        //    if (num == 0)
-        //    {
-        //        throw new ArgumentException("You can't use '0'!");
-        //    }
-            
-        //    return num;
-        //}
-
         public static int CheckInput(this string number)
         {
             int num;
